56c6c9adb30208db630b7cae024aec9fad1a68bc
Emmanuel <emmanueldceu@gmail.com>
2026-04-22 12:20:48 -0400

    Update O.U structureScript.ps1

--- O.U structureScript.ps1
+++ O.U structureScript.ps1
@@ -1,4 +1,5 @@
-New-ADOrganizationalUnit -Name "Users" -Path "DC=MangoBanana,DC=local"
-New-ADOrganizationalUnit -Name "HR" -Path "OU=Users,DC=MangoBanana,DC=local"
-New-ADOrganizationalUnit -Name "IT" -Path "OU=Users,DC=MangoBanana,DC=local"
-New-ADOrganizationalUnit -Name "Finance" -Path "OU=Users,DC=MangoBanana,DC=local"
+New-ADOrganizationalUnit -Name "CompanyUsers" -Path "DC=MangoBanana,DC=local"
+
+New-ADOrganizationalUnit -Name "HR" -Path "OU=CompanyUsers,DC=MangoBanana,DC=local"
+New-ADOrganizationalUnit -Name "IT" -Path "OU=CompanyUsers,DC=MangoBanana,DC=local"
+New-ADOrganizationalUnit -Name "Finance" -Path "OU=CompanyUsers,DC=MangoBanana,DC=local"
