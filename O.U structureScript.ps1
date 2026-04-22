387cabeab1160bf088b99e1722746216f85fa2b6
Emmanuel <emmanueldceu@gmail.com>
2026-04-22 12:17:08 -0400

    Create O.U structureScript.ps1

---
+++ O.U structureScript.ps1
@@ -0,0 +1,4 @@
+New-ADOrganizationalUnit -Name "Users" -Path "DC=MangoBanana,DC=local"
+New-ADOrganizationalUnit -Name "HR" -Path "OU=Users,DC=MangoBanana,DC=local"
+New-ADOrganizationalUnit -Name "IT" -Path "OU=Users,DC=MangoBanana,DC=local"
+New-ADOrganizationalUnit -Name "Finance" -Path "OU=Users,DC=MangoBanana,DC=local"
